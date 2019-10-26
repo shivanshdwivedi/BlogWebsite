5e9298c95062c40adc8234ebfc74484139e100f4
Shivansh Dwivedi <52450815+shivanshdwivedi@users.noreply.github.com>
2019-10-27 00:46:21 +0530

    commit

--- app.js
+++ app.js
@@ -28,7 +28,11 @@ app.get("/contact" , function(req,res){
   });
 
   app.get("/compose" , function(req,res){
-    res.render("compose", {});
+    res.render("compose");
+  });
+
+  app.post("/compose", function(req,res){
+    console.log(req.body.names);
   });
  
 
