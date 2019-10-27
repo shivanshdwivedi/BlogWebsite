6aefeda0caa4b90dfcf35558adaeb981a0132cb6
Shivansh Dwivedi <52450815+shivanshdwivedi@users.noreply.github.com>
2019-10-27 13:03:25 +0530

    commit

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req,res){
-  res.render("home" , {homeStartingContent});
+  res.render("home" , {homeStartingContent , posts});
 });
 
 app.get("/about" , function(req,res){
